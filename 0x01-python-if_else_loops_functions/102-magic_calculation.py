0abe5c5c05ffe2bada55228e9e0164c7cc92758d
michaelhun <mikhun12@gmail.com>
2023-03-07 10:32:50 -0800

    if else task

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-"""Match bytecode provided by Holberton School."""
-    if a < b:
+    if (a < b):
         return (c)
-    if c > b:
+    if (c > b):
         return (a + b)
     return (a*b - c) 
