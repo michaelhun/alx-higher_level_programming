c4720b00f6ad27ddc85f8d0fde7ee25048209d74
michaelhun <mikhun12@gmail.com>
2023-06-12 13:52:12 -0700

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
- #!/usr/bin/node
- let fun='C is fun'
- let cool='Python is cool'
- let amazing='JavaScript is amazing'
-
-
-console.log(fun);
-console.log(cool);
-console.log(amazing);
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
