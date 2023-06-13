63a1b8d5d346d691ea4b8472a10839b00fd4343d
michaelhun <mikhun12@gmail.com>
2023-06-13 14:27:41 -0700

    task 2 to 6

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle{
+    constructor(s){
+        this.size =size;
+        super(w,h);
+    }
+  }
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle{
+   
+  charPrint(c){
+  if (c === undefined) {
+    c = 'X';
+  }
+  for (let i = 0; i < this.height; i++) {
+    let s = '';
+    for (let j = 0; j < this.width; j++) {
+      s += c;
+    }
+    console.log(s);
+  }
+}
+}
+module.exports = Square;
